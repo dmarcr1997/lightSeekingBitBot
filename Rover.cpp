febdf9a6ff6ea2aa0dbccf1097277e5f4acd6e3c
Damon <dmarcr1997@gmail.com>
2021-04-09 22:49:53 -0500

    Created Temperature and light reading functions

--- Rover.cpp
+++ Rover.cpp
@@ -2,29 +2,47 @@
 #define DHTPIN 7
 #define DHTTYPE DHT11
 
-#define ML_Ctrl 13  //define the direction control pin of left motor
-#define ML_PWM 11   //define the PWM control pin of left motor
-#define MR_Ctrl 12  //define direction control pin of right motor
+#define ML_Ctrl 13 //define the direction control pin of left motor
+#define ML_PWM 11  //define the PWM control pin of left motor
+#define MR_Ctrl 12 //define direction control pin of right motor
 #define MR_PWM 3   // define the PWM control pin of right motor
- 
+
 float tempVal = 0;
 int lightInPin1 = A2;
-int lightInPin2 = A4;  
+int lightInPin2 = A4;
 int lightSensorValue1 = 0;
 int lightSensorValue2 = 0;
 DHT dht(DHTPIN, DHTTYPE);
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(ML_Ctrl, OUTPUT);
-  pinMode(ML_PWM, OUTPUT);
-  pinMode(MR_Ctrl, OUTPUT);
-  pinMode(MR_PWM, OUTPUT);
-  dht.begin();
-  Serial.begin(9600);
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(ML_Ctrl, OUTPUT);
+    pinMode(ML_PWM, OUTPUT);
+    pinMode(MR_Ctrl, OUTPUT);
+    pinMode(MR_PWM, OUTPUT);
+    dht.begin();
+    Serial.begin(9600);
 }
 
-void loop() {
-  
-  
+void loop()
+{
+}
 
+void temperature()
+{
+    Serial.println("Temperature = ");
+    Serial.println(dht.readTemperature());
+    Serial.println("Humidity = ");
+    Serial.println(dht.readHumidity());
+    delay(1000);
+}
+
+void lightValues()
+{
+    lightSensorValue1 = analogRead(lightInPin1);
+    lightSensorValue2 = analogRead(lightInPin2);
+    Serial.println(lightSensorValue1);
+    delay(200);
+    Serial.println(lightSensorValue2);
+    delay(500);
 }
